dbaa58dd0722e6ed52c1f54611a100548b31690c
TonyStark-source <5.tony.stark.29.1970@gmail.com>
2021-08-19 12:15:40 +0900

    Navbar and Save Scroll Position

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// document.addEventListener('DOMContentLoaded', function() {
-// 	var sep = '\uE000'; // an unusual char: unicode 'Private Use, First'
-
-// 	window.addEventListener('pagehide', function(e) {
-// 		window.name += sep + window.pageXOffset + sep + window.pageYOffset;
-// 	});
-
-// 	if(window.name && window.name.indexOf(sep) > -1)
-// 	{
-// 		var parts = window.name.split(sep);
-// 		if(parts.length >= 3)
-// 		{
-// 			window.name = parts[0];
-// 			window.scrollTo(parseFloat(parts[parts.length - 2]), parseFloat(parts[parts.length - 1]));
-// 		}
-// 	}
-// });
+$(document).ready(function () {
+  if (localStorage.getItem(location.pathname) != null) {
+    $(window).scrollLeft(
+      localStorage.getItem(location.pathname)
+    )
+    console.log($(window).scrollLeft())
+  }
+  
+  $(window).on('scroll', function () {
+    localStorage.setItem(
+      location.pathname,
+      $(window).scrollLeft()
+      )
+      console.log('saving...')
+      console.log($(window).scrollLeft())
+  })
+})
